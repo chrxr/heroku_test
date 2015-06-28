b477d05c875662eca8c0e910a5f11d27ccd8acf6
Chris Rogers <chrxr@hotmail.co.uk>
2015-06-28 23:04:49 +0100

    added compression

--- my_project_name/settings/production.py
+++ my_project_name/settings/production.py
@@ -8,13 +8,6 @@ env = os.environ.copy()
 
 SECRET_KEY = env['SECRET_KEY']
 
-COMPRESS_OFFLINE = True
-COMPRESS_CSS_FILTERS = [
-    'compressor.filters.css_default.CssAbsoluteFilter',
-    'compressor.filters.cssmin.CSSMinFilter',
-]
-COMPRESS_CSS_HASHING_METHOD = 'content'
-
 try:
     from .local import *
 except ImportError:
